40568dd69f962515d27a48843ea2635da496d8f6
Thomas Wojcik <thomas-wojcik@hotmail.com>
2017-05-18 16:05:33 +0200

    update
    update

--- raytracer.cs
+++ raytracer.cs
@@ -12,22 +12,21 @@ class Raytracer
 	// member variables
 	public Surface screen;
     public Vector3 screenCorner0, screenCorner1, screenCorner2;
-    public int distance;
+
+    // distance from camera to screen (change FOV by changing distance)
+    public int distance = 1;
 	// initialize
 	public void Init()
 	{
             // the camera from where you see the scene
             template.camera camera = new template.camera(new Vector3(0, 0, 0), new Vector3(0, 0, 1));
 
-            // distance from camera to screen (change POV by changing distance)
-            distance = 100;
-
             // the screen where the rays are shot at
             screen = new Surface(512, 512);
             // the corners of the screen
             screenCorner0 = camera.CameraPosition + distance * camera.CameraDirection + new Vector3(-1, -1, 0);
-            screenCorner0 = camera.CameraPosition + distance * camera.CameraDirection + new Vector3(1, -1, 0);
-            screenCorner0 = camera.CameraPosition + distance * camera.CameraDirection + new Vector3(-1, 1, 0);
+            screenCorner1 = camera.CameraPosition + distance * camera.CameraDirection + new Vector3(1, -1, 0);
+            screenCorner2 = camera.CameraPosition + distance * camera.CameraDirection + new Vector3(-1, 1, 0);
         }
 	// tick: renders one frame
 	public void Tick()
