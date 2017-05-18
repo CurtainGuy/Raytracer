129fcd61873b6af668875074ec2f3809a290d99f
Thomas Wojcik <thomas-wojcik@hotmail.com>
2017-05-18 16:36:25 +0200

    Update
    update

--- raytracer.cs
+++ raytracer.cs
@@ -5,29 +5,33 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
-namespace Template {
-
-class Raytracer
+namespace Template
 {
-	// member variables
-	public Surface screen;
 
-	// initialize
-	public void Init()
-	{
+    class Raytracer
+    {
+        // member variables
+        Camera camera;
+        // initialize
+        public void Init()
+        {
             // the camera from where you see the scene
-            template.camera camera = new template.camera(new Vector3(0, 0, 0), new Vector3(0, 0, 1));
-
-            // the screen where the rays are shot at
-            screen = new Surface(512, 512);
+            camera = new Camera(new Vector3(0, 0, 0), new Vector3(0, 0, 1));
         }
-	// tick: renders one frame
-	public void Tick()
-	{
-		screen.Clear( 0 );
-		screen.Print( "hello world", 2, 2, 0xffffff );
-        screen.Line(2, 20, 160, 20, 0xff0000);
-	}
-}
+        // tick: renders one frame
+        public void Tick()
+        {
+        }
+
+        public void Render()
+        {
+            Ray ray = new Ray(new Vector3(0, 0, 0), new Vector3(0, 0, 0), 0);
+            for (int x = 0; x < camera.screen.width; x++)
+                for (int y = 0; y < camera.screen.height; y++)
+                {
+                    //shoot rays at screen
+                }
+        }
+    }
 
 } // namespace Template
